717a14d1d35f9d8188fe5fe2662072ee47277ad6
El-Sin-Nombre <shrijaymore143@gmail.com>
2025-10-23 12:55:35 +0530

    add refreshtoken

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { loginUser, logoutUser, regitserUser } from "../controllers/user.controllers.js";
+import { loginUser, logoutUser, refreshAccessToken, regitserUser } from "../controllers/user.controllers.js";
 import {upload} from "../middlewares/multer.middleware.js"
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 const router = new Router();
@@ -21,4 +21,6 @@ router.route('/login').post(loginUser)
 
 //secured routes
 router.route('/logout').post(verifyJWT,logoutUser)
+
+router.route('/refresh-token').post(refreshAccessToken);
 export default router;
